22b069b3212eec278f2a3d66fcba188668c2e043
Andrew2009 <andrewsagraves@gmail.com>
2015-11-12 13:04:19 -0600

    working on firebase(deleting from firebase)

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -5,22 +5,35 @@ requirejs.config({
 		"hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
 	  "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
     // "lodash": "../lib/bower_components/lodash/lodash.min"
+    "firebase": "../lib/bower_components/firebase/firebase"
   },
 
   shim: {
       "bootstrap": {
         "deps" : ["jquery"]
+      },
+      "firebase": {
+        exports: "Firebase"
       }
     }
 });
-require(["bootstrap", "hbs", "get-songs", "mainAdd", "logsong", "script", "dom-songs"], 
+require(["firebase", "eventHandlers", "bootstrap", "hbs", "get-songs", "mainAdd", "logsong", "script", "dom-songs"], 
 
-  function (bootstrap, hbs, mySongs, addFirebase, logSong, script, toDom){
-  	console.log("working");
+  function (Firebase, eventHandlers, bootstrap, hbs, mySongs, addFirebase, logSong, script, toDom){
+  	// console.log("working");
+
+    var allSongsObject = {};
+    var allSongsArray = [];
+    var originalSongsArray = [];
+
+
 
     mySongs.getSongs(toDom.addSongToDom);
 
     $("button1").on("click", logSong.addSongToList);
+
+     // Initialize the event handlers
+    eventHandlers.init({songArray: originalSongsArray});
     
 
 
@@ -64,16 +77,58 @@ require(["bootstrap", "hbs", "get-songs", "mainAdd", "logsong", "script", "dom-s
           // mySongs.getSongs(toDom.filterSelectedArtists);
       }
       toDom.addSongToDom(filtersong);
-      
-        // {
-        //   songs: {}
-        // }
       });
     });
-  });
 
-	// });
+
 	
+
+
+
+// *********firebase********
+
+// function firebase() {}
+
+// Create a reference to your Firebase database
+
+var firebase = new Firebase("https://scorching-torch-9452.firebaseio.com/");
+
+      // Listen for when anything changes on the "songs" key
+      firebase.child("songs").on("value", function(snapshot) {
+
+      // Store the entire songs key in a local variable
+      var allSongsObject = snapshot.val();
+
+      // Bind the song object to the song list template
+      $("#songList").html(toDom.addSongToDom(allSongsObject));
+      console.log(toDom);
+
+      // Make an array of unique artist names
+      function unique(){
+        return unique;
+      }
+      var uniqueArtists = unique(allSongsObject).uniqueArtists;
+
+      // Bind the unique artists to the filteredArtists template
+      $("#artistselect").html(toDom.addSongToDom({artists:uniqueArtists }));
+
+      // Make an array of unique album names
+      var uniqueAlbums = unique(allSongsObject).uniqueAlbums;
+
+      // Bind the unique albums to the filteredAlbums template
+      $("#albumselect").html(toDom.addSongToDom({albums:uniqueAlbums }));
+
+      // Update event handlers with new data
+      eventHandlers.updateSongs(originalSongsArray);
+
+    });
+
+    // Initialize the Material Bootstrap plugin
+    // $.material.init();
+
+
+});
+
 
 // {{!-- ********Lodash******** --}}
   // var matchingChoices = _.chain($("#div-2"))
